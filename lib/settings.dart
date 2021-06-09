985283b1f6e969b71842fe84b8b8e646dc4d1ec2
Shubham Prakash <chimps.shubham2373@gmail.com>
2021-06-09 18:01:08 +0530

    added routeMap and updatable callback methods

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,30 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+
+import 'package:google_fonts/google_fonts.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
 
 // settings page NEW made variables, might be available in constants already.
 const kBorderColor = Color(0xFFD9DCE3);
 const kDialogButtonTitleColor = Color(0xff88889D);
 const kOrangeAccentColor = Color(0xFFFF7648);
+
 /// [kMultiplyFactor] is used to convert design hard values(margin, padding, size, borders, etc)
 /// into screen specific values.
-double kMultiplyFactor = 1;   
-TextStyle kSettingsTileTextStyle = TextStyle(
+double kMultiplyFactor = 1;
+TextStyle kSettingsTileTextStyle = GoogleFonts.poppins(
   fontSize: 13 * kMultiplyFactor,
   color: kHomeHeaderTextColour,
-  fontFamily: 'Poppins',
   fontWeight: FontWeight.w400,
 );
+
+/// [routeURLs] is the used for mapping 
+Map<String, String> routeURLs = {
+  // 'Notifications':  " No url for Notifications",
+  // 'Feedback' : "No url for FeedbackUI() screen",
+  'Terms & Conditions':
+      "https://spicelearn.inroad.in/local/staticpage/view.php?page=terms-and-conditions",
+  'Privacy Policy':
+      "https://spicelearn.inroad.in/local/staticpage/view.php?page=privacy-policy",
+  'Help': "https://spicelearn.inroad.in/local/staticpage/view.php?page=help",
+};
 //
 
 // already in the constants file; copied from global constants file
 const kBackgroundColour = Color(0xFFF2F4FB);
 const kHomeHeaderTextColour = Color(0xFF212523);
 
-TextStyle kMyCoursesHeaderStyle = TextStyle(
+TextStyle kMyCoursesHeaderStyle = GoogleFonts.poppins(
   fontSize: 18 * kMultiplyFactor,
   color: kHomeHeaderTextColour,
-  fontFamily: 'Poppins',
   fontWeight: FontWeight.w600,
 );
 //
@@ -48,30 +60,36 @@ class Settings extends StatelessWidget {
               child: Column(
                 children: [
                   SettingsTile(
-                    buttonTitle: 'Notifications',
+                    title: 'Notifications',
                     showToggleSwitch: true,
-                    onPressed: () {
-                      print('Extra action injected');
+                    switchToggleAction: () {
+                      print('Extra Toggle Action Ran!');
                     },
                   ),
                   SettingsTile(
-                    buttonTitle: 'Feedback',
+                    title: 'Feedback',
                   ),
                   SettingsTile(
-                    buttonTitle: 'Terms & Conditions',
+                    title: 'Terms & Conditions',
                   ),
                   SettingsTile(
-                    buttonTitle: 'Privacy Policy',
+                    title: 'Privacy Policy',
                   ),
                   SettingsTile(
-                    buttonTitle: 'Help',
+                    title: 'Help',
                   ),
                 ],
               ),
             ),
             Spacer(),
             LogoutButton(
-              onPressed: () => onLogOutPressed(context).show(),
+              onPressed: () => onLogOutPressed(
+                context: context,
+                logOutConfirmationAction: () {
+                  Navigator.pop(context);
+                  Navigator.pop(context);
+                },
+              ).show(),
             ),
           ],
         ),
@@ -116,16 +134,14 @@ class CustomAppBar extends StatelessWidget implements PreferredSize {
 
 //settings options tile component
 class SettingsTile extends StatefulWidget {
-  final String buttonTitle;
-  final Function onPressed;
+  final String title;
   final bool showToggleSwitch;
-
-  static void _noActionMethod() {}
+  final Function? switchToggleAction;
 
   const SettingsTile({
-    required this.buttonTitle,
-    this.onPressed = _noActionMethod,
+    required this.title,
     this.showToggleSwitch = false,
+    this.switchToggleAction,
   });
 
   @override
@@ -142,8 +158,9 @@ class _SettingsTileState extends State<SettingsTile> {
       onTap: widget.showToggleSwitch
           ? () {} //no action on tapping tile body if Switch avaliable
           : () {
-              print('${widget.buttonTitle} pressed!');
-              widget.onPressed();
+              print('${widget.title} pressed!');
+              onSettingTilePressed(
+                  context, routeURLs[widget.title] ?? 'Default');
             },
       child: Container(
         height: 64 * kMultiplyFactor,
@@ -160,7 +177,7 @@ class _SettingsTileState extends State<SettingsTile> {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Text(
-              widget.buttonTitle,
+              widget.title,
               style: kSettingsTileTextStyle,
             ),
             widget.showToggleSwitch
@@ -168,11 +185,11 @@ class _SettingsTileState extends State<SettingsTile> {
                     value: switchValue,
                     onChanged: (bool value) {
                       print('toggleButton pressed!');
-                      // add extra switch functionality here to onPressed property;
                       setState(() {
                         switchValue = value;
                       });
-                      widget.onPressed();
+                      // add switch functionality here to onPressed property;
+                      widget.switchToggleAction!();
                     },
                     // onChanged: (_) {},
                     activeColor: kOrangeAccentColor,
@@ -207,10 +224,10 @@ class LogoutButton extends StatelessWidget {
       child: Center(
         child: Text(
           'Log Out',
-          style: TextStyle(
+          style: GoogleFonts.poppins(
             color: kOrangeAccentColor,
             fontSize: 13 * kMultiplyFactor,
-            fontWeight: FontWeight.w300,
+            fontWeight: FontWeight.w400,
           ),
         ),
       ),
@@ -220,7 +237,9 @@ class LogoutButton extends StatelessWidget {
 
 // on pressing logout button; show alert box
 // using rflutter_alert package for custom alert box
-Alert onLogOutPressed(BuildContext context) {
+Alert onLogOutPressed(
+    {required BuildContext context,
+    required VoidCallback logOutConfirmationAction}) {
   // Dialog Button Widget abstraction for repeated use
   // used inside Alert widget
   DialogButton templateDialogButton({
@@ -236,7 +255,7 @@ Alert onLogOutPressed(BuildContext context) {
         color: color,
         child: Text(
           title,
-          style: TextStyle(
+          style: GoogleFonts.poppins(
             color: titleColor,
             fontSize: 16 * kMultiplyFactor,
           ),
@@ -259,9 +278,11 @@ Alert onLogOutPressed(BuildContext context) {
         ),
       ),
       buttonsDirection: ButtonsDirection.column,
-      titleStyle: kMyCoursesHeaderStyle.copyWith(
+      titleStyle: GoogleFonts.poppins(
+        color: kHomeHeaderTextColour,
         fontSize: 20 * kMultiplyFactor,
         fontWeight: FontWeight.w400,
+        letterSpacing: -1 * kMultiplyFactor,
       ),
     ),
     // type: AlertType.warning,
@@ -269,12 +290,10 @@ Alert onLogOutPressed(BuildContext context) {
     // desc: "Are you sure you want \nto Log Out?",
     buttons: [
       templateDialogButton(
-          title: "Log Out",
-          color: kOrangeAccentColor,
-          onPressed: () {
-            Navigator.pop(context);
-            Navigator.pop(context);
-          }),
+        title: "Log Out",
+        color: kOrangeAccentColor,
+        onPressed: logOutConfirmationAction,
+      ),
       templateDialogButton(
           title: "Cancel",
           titleColor: kDialogButtonTitleColor,
@@ -282,5 +301,24 @@ Alert onLogOutPressed(BuildContext context) {
             Navigator.pop(context);
           }),
     ],
+  );
+}
+
+/// Use [onSettingTilePressed] method for opening the webView for respective tile
+/// Update return type accordingly.
+/// routeURL can be retrieved by your already created [getURL] method.
+void onSettingTilePressed(BuildContext context, String routeURL) {
+  Navigator.of(context).push(
+    MaterialPageRoute(
+      // builder: (BuildContext context) => WebviewforSettings(getURL(3), 1),
+      builder: (BuildContext context) => routeURL == 'Default'
+          //Replace this Scaffold with Feedback Page
+          ? Scaffold(
+              body: Center(child: Text('Special route for Feedback')),
+            )
+          : Scaffold(
+              body: Center(child: Text(routeURL)),
+            ),
+    ),
   );
 }
